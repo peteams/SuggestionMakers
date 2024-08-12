1c4c23b5a51127d9ac1224bf41650df6776e1634
Pete Ansell <me@peteansell.com>
2024-08-11 18:13:28 -0700

    Add project files.

---
+++ SuggestionMakers/ISynchronousSuggestionMaker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace SuggestionMakers
+{
+    /// <summary>
+    /// Synchonous interface to a suggestion maker.
+    /// </summary>
+    public interface ISynchronousSuggestionMaker
+    {
+        /// <summary>
+        /// Request a sequence of suggestions based on <paramref name="start"/>.
+        /// </summary>
+        /// <param name="start">A common prefix for the suggestions to be returned. The 
+        /// IsTokenized property of the suggestion text will be true.</param>
+        /// <param name="desiredCount">The desired number of suggestions to be returned 
+        /// from the function. The client is likely to try and retrieve at least this 
+        /// number of suggestions and may attempt to retrieve additional ones.</param>
+        /// <returns>A enumeration of <see cref="SuggestionText"/> suggestions. The 
+        /// returned suggestions should be suffixes to the <paramref name="start"/> text, 
+        /// so if the input parameter is "Hello" an enumerated return may be " world" 
+        /// (with a leading space). The client will cope with enumerated 
+        /// <see cref="SuggestionText"/> values that have false IsTokenized 
+        /// properties.</returns>
+        /// <remarks>A <paramref name="start"/> value of 
+        /// <see cref="SuggestionText.Empty"/> is used when starting a new 
+        /// sentence.</remarks>
+        IEnumerable<SuggestionText> CreateCompletionSuggestions(in SuggestionText start, 
+            in int desiredCount);
+
+        /// <summary>
+        /// A hint given to the <see cref="ISynchronousSuggestionMaker"/> implementation
+        /// that the given text was used by the user.
+        /// </summary>
+        /// <param name="text">The text the user accepted.</param>
+        /// <remarks>The implementator is free to ignore this text or may record it as an
+        /// example of the kind of text the user accepts.
+        /// <para>The implementation may ignore this call.</para></remarks>
+        void RecordAcceptedSuggestionHint(in SuggestionText text);
+
+        /// <summary>
+        /// A hint given to the <see cref="ISynchronousSuggestionMaker"/> implementation
+        /// of a piece of text to which the user is likely to repsond.
+        /// </summary>
+        /// <param name="text">The text the user is likely to respond to.</param>
+        /// <remarks>The suggestions may be being used as part of a two- or multi-way 
+        /// conversation. If the other side or sides of the conversation are known to the
+        /// client, it will used this call to provide the 
+        /// <see cref="ISynchronousSuggestionMaker"/> of that context.
+        /// <para>The implementation may ignore this call.</para></remarks>
+        void RecordExternalTextHint(in SuggestionText text);
+    }
+}
---
+++ SuggestionMakers/SuggestionText.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SuggestionMakers
+{
+    /// <summary>
+    /// Container for text that forms part of a conversation.
+    /// </summary>
+    public struct SuggestionText
+    {
+        private readonly string _text;
+        private readonly int[] _tokenEnds;
+
+        private SuggestionText(in string text, in int[] tokenEnds)
+        {
+            _text = text;
+            _tokenEnds = tokenEnds;
+        }
+
+        /// <summary>
+        /// Create an untokenized <see cref="SuggestionText"/>.
+        /// </summary>
+        /// <param name="text">The text to be used.</param>
+        /// <returns>A <see cref="SuggestionText"/> that has <see cref="IsTokenized"/> 
+        /// false.</returns>
+        public static SuggestionText CreateUntokenized(in string text)
+        {
+            Debug.Assert(text is object);
+
+            var suggestionText = new SuggestionText(text, null);
+            return suggestionText;
+        }
+
+        /// <summary>
+        /// Create a tokenized <see cref="SuggestionText"/>.
+        /// </summary>
+        /// <param name="text">The text to be used.</param>
+        /// <param name="tokenEnds">An non-zero integers that mark the end of each token 
+        /// within the string. The last item in the array should equal the length of the 
+        /// string.</param>
+        /// <returns>A <see cref="SuggestionText"/> that has <see cref="IsTokenized"/> 
+        /// false.</returns>
+        public static SuggestionText CreateTokenized(in string text, in int[] tokenEnds)
+        {
+            AssertValid(text, tokenEnds);
+
+            var suggestionText = new SuggestionText(text, tokenEnds);
+            return suggestionText;
+        }
+
+        /// <summary>
+        /// An empty tokenize instance of <see cref="SuggestionText"/>.
+        /// </summary>
+        public static SuggestionText Empty { get; } = 
+            new SuggestionText(string.Empty, new[] { 0 });
+
+        /// <summary>
+        /// The text contained in this object.
+        /// </summary>
+        public string Text => _text;
+
+        /// <summary>
+        /// True when the contained text has stored token end markers.
+        /// </summary>
+        public bool IsTokenized => _tokenEnds is object;
+
+        /// <summary>
+        /// If <see cref="IsTokenized"/> is true, the token end values passed to 
+        /// <see cref="CreateTokenized(in string, in int[])"/>.
+        /// </summary>
+        public IReadOnlyList<int> TokenEnds => _tokenEnds;
+
+        [Conditional("DEBUG")]
+        private static void AssertValid(in string text, in int[] tokenEnds)
+        {
+            Debug.Assert(text is object);
+            Debug.Assert(tokenEnds is object);
+
+            var prevEnd = 0;
+            foreach (var thisEnd in tokenEnds)
+            {
+                Debug.Assert(prevEnd < thisEnd);
+                prevEnd = thisEnd;
+            }
+
+            Debug.Assert(prevEnd == text.Length);
+        }
+    }
+}
